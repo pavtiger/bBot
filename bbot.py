5cebef4864b987c66b81a9580938ecd0a5d09c3a
pavTiger <pavTiger@gmail.com>
2019-10-28 22:08:01 +0300

    subjects input

--- bbot.py
+++ bbot.py
@@ -22,25 +22,38 @@ def send(mess):
         message=mess, random_id=randint(0, 214748647))
 
 hometask = {}
-
+sub = ''
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         event.text = event.text.lower()
         if read == True:
-            text = event.text.split(';')
-            send(text[0])
-            hometask[text[0]] = text[1]
+            sub = event.text  hometask[text[0]] = text[1]
+            if sub = 
             
-        if event.text == 'start' or event.text == 'начать' or event.text == 'начало':
+        elif event.text == 'start' or event.text == 'начать' or event.text == 'начало':
+            read = False
             event.text=event.text.lower()
             vk = vk_session.get_api()
             StartKeyboard = VkKeyboard(one_time=True)
-            StartKeyboard.add_button('Поменять Настройки', color=VkKeyboardColor.NEGATIVE)
+            StartKeyboard.add_button('посмотреть дз', color=VkKeyboardColor.POSITIVE)
             StartKeyboard.add_button('добавить дз', color=VkKeyboardColor.POSITIVE)
-            send('йоу')
+            send('хей')
+            
         elif event.text == "добавить дз":
-            send("<subject>; txt")
-            read = True;
+            StartKeyboard.add_button('русский', color=VkKeyboardColor.NEGATIVE)
+            StartKeyboard.add_button('алгебра', color=VkKeyboardColor.NEGATIVE)
+            StartKeyboard.add_button('', color=VkKeyboardColor.NEGATIVE)
+            StartKeyboard.add_button('', color=VkKeyboardColor.NEGATIVE)
+            StartKeyboard.add_button('другой вариант', color=VkKeyboardColor.NEGATIVE)
+            send("<предмет>")
+            read = True
+            
+        elif event.text == "посмотреть дз" or event.text == "hw":
+            StartKeyboard = VkKeyboard(one_time=True)
+            for key in hometask.keys():
+                StartKeyboard.add_button(hometask[key], color=VkKeyboardColor.POSITIVE)
+            send('вотъ')
+            
         else:
             send("errorr")
